1ea7cac6570f1a4069b8fc45f7b9107d9418c4f3
chude <chudesya@gmail.com>
2024-05-30 15:38:04 -0700

    add new .gitignore

--- src/test/java/TestTest.java
+++ src/test/java/TestTest.java
@@ -1,8 +1,16 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestTest {
     @Test
     public void testTest(){
 
+        Assert.assertTrue(true);
+
+    }
+
+    @Test
+    public void testFail() {
+        Assert.assertTrue(false);
     }
 }
