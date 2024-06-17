99b3281c19d5c9a05a3f6294fe62288c0cbb29f1
Olesia V Budimovich <130872644+chudesya@users.noreply.github.com>
2024-06-17 14:57:14 -0700

    renew pom, yml (#23)
    * spelling check, renew pom, yml, Add SearchBarTest

--- src/test/java/com/lumatest/base/BaseTest.java
+++ src/test/java/com/lumatest/base/BaseTest.java
@@ -4,32 +4,39 @@ import com.lumatest.utils.DriverUtils;
 import com.lumatest.utils.ReportUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
 import org.testng.ITestResult;
 import org.testng.Reporter;
 import org.testng.annotations.*;
 
 public abstract class BaseTest {
     private WebDriver driver;
+    private final ThreadLocal<WebDriver> threadLocalDriver = new ThreadLocal<>();
 
     @BeforeSuite
     protected void setupWebDriverManager() {
         WebDriverManager.chromedriver().setup();
         WebDriverManager.firefoxdriver().setup();
-//       WebDriverManager.edgedriver().setup();
-//        WebDriverManager.operadriver().setup();
-//        WebDriverManager.chromiumdriver().setup();
-//        WebDriverManager.iedriver().setup();
+//      WebDriverManager.edgedriver().setup();
+//      WebDriverManager.operadriver().setup();
+//      WebDriverManager.chromiumdriver().setup();
+//      WebDriverManager.iedriver().setup();
     }
 
     @Parameters("browser")
-    @BeforeMethod()
-    protected void setupDriver(@Optional("chrome") String browser, ITestResult result) {
-        Reporter.log("_______________________________________________", true);
-        Reporter.log("RUN: " + result.getMethod().getMethodName(), true);
+    @BeforeMethod(alwaysRun = true)
+    protected void setupDriver(@Optional("chrome") String browser, ITestContext context, ITestResult result) {
+        Reporter.log("______________________________________________________________________", true);
 
         this.driver = DriverUtils.createDriver(browser, this.driver);
+        this.threadLocalDriver.set(driver);
+
+        Reporter.log("Test Thread ID: " + Thread.currentThread().getId(), true);
+        Reporter.log("TEST SUIT: " + context.getCurrentXmlTest().getSuite().getName(), true);
+        Reporter.log("RUN " + result.getMethod().getMethodName(), true);
+
         if (getDriver() == null) {
-            Reporter.log("ERROR: Unknown parameter 'browser' - " + browser + ".", true);
+            Reporter.log("ERROR: Unknown parameter 'browser' - '" + browser + "'.", true);
 
             System.exit(1);
         }
@@ -40,21 +47,24 @@ public abstract class BaseTest {
     @Parameters("browser")
     @AfterMethod(alwaysRun = true)
     protected void tearDown(@Optional("chrome") String browser, ITestResult result) {
-        Reporter.log("PASS: " + result.getMethod().getMethodName() + " : " + ReportUtils.getTestStatus(result), true);
+        Reporter.log(result.getMethod().getMethodName() + ": " + ReportUtils.getTestStatus(result),
+                true);
 
-        if (this.driver != null) {
+        if (getDriver() != null) {
             getDriver().quit();
             Reporter.log("INFO: " + browser.toUpperCase() + " driver closed.", true);
 
-            this.driver = null;
+            Reporter.log("After Test Thread ID: " + Thread.currentThread().getId(), true);
+            threadLocalDriver.remove();
+
+            driver = null;
+
         } else {
-
-            Reporter.log("INFO: Driver is NULL .", true);
-
+            Reporter.log("INFO: Driver is null.", true);
         }
     }
 
     protected WebDriver getDriver() {
-        return this.driver;
+        return threadLocalDriver.get();
     }
 }
--- src/test/java/com/lumatest/utils/DriverUtils.java
+++ src/test/java/com/lumatest/utils/DriverUtils.java
@@ -1,21 +1,20 @@
 package com.lumatest.utils;
 
+import java.util.Map;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chromium.ChromiumOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-
-import java.util.Map;
-
 
 public class DriverUtils {
     private static final ChromeOptions chromeOptions;
     private static final FirefoxOptions firefoxOptions;
+    private static final ChromiumOptions<ChromeOptions> chromiumOptions;
 
     static {
         chromeOptions = new ChromeOptions();
-
         chromeOptions.addArguments("--incognito");
         chromeOptions.addArguments("--headless");
         chromeOptions.addArguments("--window-size=1920,1080");
@@ -27,28 +26,38 @@ public class DriverUtils {
         chromeOptions.addArguments("--ignore-certificate-errors");
 
         firefoxOptions = new FirefoxOptions();
-
         firefoxOptions.addArguments("--incognito");
         firefoxOptions.addArguments("--headless");
         firefoxOptions.addArguments("--window-size=1920,1080");
         firefoxOptions.addArguments("--disable-gpu");
         firefoxOptions.addArguments("--no-sandbox");
         firefoxOptions.addArguments("--disable-dev-shm-usage");
-        firefoxOptions.addArguments("--disable-web-security");
-        firefoxOptions.addArguments("--allow-running-insecure-content");
-        firefoxOptions.addArguments("--ignore-certificate-errors");
-    }
 
+        chromiumOptions = chromeOptions;
+    }
 
     private static WebDriver createChromeDriver(WebDriver driver) {
         if (driver != null) {
             driver.quit();
         }
         ChromeDriver chromeDriver = new ChromeDriver(chromeOptions);
+        chromeDriver.executeCdpCommand("Network.enable", Map.of());
         chromeDriver.executeCdpCommand(
-                "Network.enable", Map.of());
+                "Network.setExtraHTTPHeaders", Map.of("headers", Map.of("accept-language", "en-US,en;q=0.9"))
+        );
+
+        return chromeDriver;
+    }
+
+    private static WebDriver createChromiumDriver(WebDriver driver) {
+        if (driver != null) {
+            driver.quit();
+        }
+        ChromeDriver chromeDriver = new ChromeDriver((ChromeOptions) chromiumOptions);
+        chromeDriver.executeCdpCommand("Network.enable", Map.of());
         chromeDriver.executeCdpCommand(
-                "Network.setExtraHTTPHeaders", Map.of("headers", Map.of("accept-language", "en-US,en;q=0.9")));
+                "Network.setExtraHTTPHeaders", Map.of("headers", Map.of("accept-language", "en-US,en;q=0.9"))
+        );
 
         return chromeDriver;
     }
@@ -57,13 +66,8 @@ public class DriverUtils {
         if (driver != null) {
             driver.quit();
         }
-        FirefoxDriver firefoxDriver = new FirefoxDriver(firefoxOptions);
-//        firefoxDriver.executeCdpCommand(
-//                "Network.enable", Map.of());
-//        firefoxDriver.executeCdpCommand(
-//                "Network.setExtraHTTPHeaders", Map.of("headers", Map.of("accept-language", "en-US,en;q=0.9")));
 
-        return firefoxDriver;
+        return new FirefoxDriver(firefoxOptions);
     }
 
     public static WebDriver createDriver(String browser, WebDriver driver) {
@@ -73,6 +77,9 @@ public class DriverUtils {
             }
             case "firefox" -> {
                 return createFirefoxDriver(driver);
+            }
+            case "chromium" -> {
+                return createChromiumDriver(driver);
             }
             default -> {
                 return null;
